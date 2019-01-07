e182337f872b93dbeffb09f2ea7c79a4340e376e
Daniel Flores <d.flores042@gmail.com>
2019-01-06 22:00:08 -0500

    add form validation

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ function saveBookmark(e){
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -25,6 +29,10 @@ function saveBookmark(e){
     setBookmarks(bookmarks);
   }
 
+  // reset form
+  document.getElementById('myForm').reset();
+
+  // re-fetch bookmarks
   fetchBookmarks();
 }
 
@@ -74,4 +82,21 @@ function setBookmarks(bookmarks){
 
 function getBookmarks(){
   return JSON.parse(localStorage.getItem('bookmarks'))
+}
+
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
 }
